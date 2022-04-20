d1ec5193e2c09567e231a63c3464c86a06e62fdb
pranav <pranavpk404@gmail.com>
2022-04-20 08:43:00 +0530

    removed bg from logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,7 @@ const Navbar = ({ darkTheme, setDarkTheme }) => {
     <nav className="p-5 flex flex-wrap sm:justify-between justify-center items-center ">
       <div className="flex justify-between items-center space-x-5 w-screen">
         <Link to="/">
-          <p className="text-2xl rounded-md bg-slate-200 p-1 text-black dark:text-white dark:bg-slate-600">
-            Radon
-          </p>
+          <p className="text-2xl ">Radon</p>
         </Link>
         <button
           className="text-xl  bg-white rounded-full p-2
